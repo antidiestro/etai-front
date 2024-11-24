efe718411e2f5a26a3a5231b26d5587f3072bb34
rené <rene.morales.sanchez@gmail.com>
2024-11-24 08:50:17 -0300

    feat: adjust styles, show cool emojis

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppContext } from "../contexts/AppContext";
 import preventWidows from "../scripts/preventWidows";
 import Suggestions from "./Suggestions";
@@ -7,13 +7,71 @@ export default function Search() {
   const { setQuery } = useAppContext();
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const emojis = [
+    "🧬",
+    "🔬",
+    "🧪",
+    "🦠",
+    "🧮",
+    "📊",
+    "🔭",
+    "🩻",
+    "🫀",
+    "🧠",
+    "🦿",
+    "🔋",
+    "💊",
+    "🧲",
+    "🩺",
+    "🦷",
+    "🔍",
+    "💡",
+    "🎯",
+    "🧩",
+    "🧯",
+    "🌡️",
+    "⚖️",
+    "🧶",
+    "🧵",
+    "🩸",
+    "🧼",
+    "🧪",
+    "📡",
+    "🛰️",
+    "🌍",
+    "🌋",
+    "🌊",
+    "🌪️",
+    "☄️",
+    "💫",
+    "🌌",
+    "🪐",
+    "🌓",
+    "🦕",
+    "🧱",
+  ];
+
+  const [activeEmojiIndex, setActiveEmojiIndex] = useState(0);
+
+  const activeEmoji = emojis[activeEmojiIndex];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveEmojiIndex((prev) => (prev + 1) % emojis.length);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [emojis.length]);
+
   return (
     <div className="flex-1 flex flex-col justify-center -mt-20">
       <div className="mb-6 sm:mb-10">
-        <div className="text-orange-600 text-[32px] leading-tight sm:text-center">
-          {preventWidows("What do you want to learn today?") + " 🤓"}
+        <div className="text-orange-600 text-[32px] sm:text-[48px] tracking-tight leading-tight font-medium sm:text-center">
+          {preventWidows("What do you want to learn today?") +
+            " " +
+            activeEmoji}
         </div>
-        <div className="mx-auto max-w-sm sm:text-center mt-3">
+        <div className="mx-auto max-w-sm sm:text-lg sm:leading-7 sm:text-center mt-3">
           {preventWidows(
             "Search, browse and understand academic literature with AI-generated summaries."
           )}
